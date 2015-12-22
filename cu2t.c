0bfd37505ef7cd29869c633adf03cba54f244362
Cian Synnott <cian@emauton.org>
2015-12-22 13:11:24 +0000

    Clean up, reduce & comment the code. Handle EPIPE.

--- cu2t.c
+++ cu2t.c
@@ -6,17 +6,13 @@
  * line protocol.
  *
  * The operator can run as many processes as they need to balance incoming
- * UDP traffic against TCP backends. Using SO_REUSEPORT on the UDP socket
- * means that the kernel manages the queueing and switching of datagrams into
- * the bridge processes. See https://lwn.net/Articles/542629/.
+ * UDP traffic against TCP backends.
  *
  * Example:
  *     cu2t localhost 2003 backend 2003
  * forwards Carbon data from UDP localhost:2003 to TCP backend:2003.
  *
- * TODO(emauton): handle signals, print stats to stdout every 5s or so, maybe
- *                reconnect on failure (though allowing whatever it is that's
- *                supervising this to just restart is probably wiser).
+ * TODO(emauton): print stats to stdout every 5s or so
  * */
 
 #include <arpa/inet.h>
@@ -29,23 +25,138 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define BUF_SIZE 1024
 
-void usage(void);
-struct addrinfo *get_addr(char *host, char *port, int socktype);
-int bind_udp_sock(char *host, char *port);
-int connect_tcp_sock(char *host, char *port);
-void send_or_die(int fd, char *buf, size_t len);
+/* Bind a UDP listen socket to `addr'.
+ *
+ * Using SO_REUSEPORT on the UDP socket means that the kernel manages the
+ * queueing and switching of datagrams into the bridge processes. See
+ * https://lwn.net/Articles/542629/.
+ *
+ * See the `attach_socket' argument of `setup_socket()'. */
+int udp_listener(struct addrinfo *addr) {
+    int fd, optval, err;
+
+    fd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+
+    if (fd == -1) {
+        perror("udp_listener socket()");
+        return -1;
+    }
+
+    optval = 1;
+    err = setsockopt(fd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval));
+    if (err != 0) {
+        perror("udp_listener setsockopt()");
+        return -1;
+    }
+
+    if (bind(fd, addr->ai_addr, addr->ai_addrlen) == 0) {
+        return fd;
+    } else {
+        perror("udp_listener bind()");
+        close(fd);
+        return -1;
+    }
+}
+
+
+/* Connect a TCP client socket to `addr'.
+ *
+ * See the `attach_socket' argument of `setup_socket()' */
+int tcp_client(struct addrinfo *addr) {
+    int fd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+
+    if (fd == -1) {
+        perror("udp_listener socket()");
+        return -1;
+    }
+
+    if (connect(fd, addr->ai_addr, addr->ai_addrlen) == 0) {
+        return fd;
+    } else {
+        perror("tcp_listener connect()");
+        close(fd);
+        return -1;
+    }
+}
+
+
+/* Resolve an address and attach a socket to it.
+ *
+ * `socktype' is SOCK_DGRAM or SOCK_STREAM.
+ *
+ * The `attach_socket' function takes an address, creates a socket with
+ * whatever options, and connects (or binds) it to the resolved address given,
+ * returning the socket on success or -1 on error.
+ *
+ * Addresses are tried in the order returned by getaddrinfo(). */
+int setup_socket(char *host, char *port, int socktype,
+                 int (*attach_socket)(struct addrinfo *addr)) {
+    struct addrinfo *result, *p;
+    struct addrinfo hints = {0};
+    int fd, err;
+
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = socktype;
+
+    err = getaddrinfo(host, port, &hints, &result);
+    if (err != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(err));
+        exit(EXIT_FAILURE);
+    }
+
+    for (p = result; p != NULL; p = p->ai_next) {
+        fd = attach_socket(p);
+        if (fd != -1)
+            break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "failed to attach %s:%s\n", host, port);
+        exit(EXIT_FAILURE);
+    }
+
+    freeaddrinfo(result);
+    return fd;
+}
+
+
+/* Send all data in `buf' to `fd', dying on error.
+ *
+ * Note MSG_NOSIGNAL: this avoids needing to set a SIG_IGN handler for SIGPIPE
+ * in order to have EPIPE delivered correctly on socket disconnection. */
+void send_or_die(int fd, char *buf, size_t len) {
+    ssize_t nw;
+
+    while (len != 0 && (nw = send(fd, buf, len, MSG_NOSIGNAL)) != 0) {
+        if (nw == -1) {
+            if (errno == EINTR)
+                continue;
+            else {
+                fprintf(stderr, "writing to fd %d: %s\n", fd, strerror(errno));
+                exit(EXIT_FAILURE);
+            }
+        }
+        len -= nw;
+        buf += nw;
+    }
+}
+
+
+#define BUF_SIZE 1024
 
 int main(int argc, char *argv[]) {
     int udp, tcp, n;
     char buf[BUF_SIZE];
 
-    if (argc != 5)
-        usage();
+    if (argc != 5) {
+        fprintf(stderr,
+                "Usage: cu2t <udp_host> <udp_port> <tcp_host> <tcp_port>\n");
+        exit(EXIT_FAILURE);
+    }
 
-    udp = bind_udp_sock(argv[1], argv[2]);
-    tcp = connect_tcp_sock(argv[3], argv[4]);
+    udp = setup_socket(argv[1], argv[2], SOCK_DGRAM, udp_listener);
+    tcp = setup_socket(argv[3], argv[4], SOCK_STREAM, tcp_client);
 
     while (1) {
         n = recv(udp, buf, BUF_SIZE, 0);
@@ -60,112 +171,3 @@ int main(int argc, char *argv[]) {
 
     return EXIT_SUCCESS;
 }
-
-void usage(void) {
-    fprintf(stderr,
-            "Usage: cu2t <udp_host> <udp_port> <tcp_host> <tcp_port>\n");
-    exit(EXIT_FAILURE);
-}
-
-struct addrinfo *get_addr(char *host, char *port, int socktype) {
-    struct addrinfo *ret;
-    struct addrinfo hints;
-    int err;
-
-    memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = socktype;
-
-    err = getaddrinfo(host, port, &hints, &ret);
-    if (err != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(err));
-        exit(EXIT_FAILURE);
-    }
-
-    return ret;
-}
-
-int bind_udp_sock(char *host, char *port) {
-    struct addrinfo *result, *rp;
-    int fd, optval, err;
-
-    result = get_addr(host, port, SOCK_DGRAM);
-
-    /* Cf. getaddrinfo(3). */
-    for (rp = result; rp != NULL; rp = rp->ai_next) {
-        fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-        if (fd == -1) {
-            perror("bind_udp_sock socket()");
-            continue;
-        }
-
-        optval = 1;
-        err = setsockopt(fd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval));
-        if (err != 0) {
-            perror("bind_udp_sock setsockopt()");
-            continue;
-        }
-
-        if (bind(fd, rp->ai_addr, rp->ai_addrlen) == 0)
-            break;                  /* Success */
-
-        perror("bind_udp_sock bind()");
-        close(fd);
-    }
-
-    if (rp == NULL) {               /* No address succeeded */
-        fprintf(stderr, "Could not bind %s:%s\n", host, port);
-        exit(EXIT_FAILURE);
-    }
-
-    freeaddrinfo(result);
-    return fd;
-}
-
-int connect_tcp_sock(char *host, char *port) {
-    struct addrinfo *result, *rp;
-    int fd;
-
-    result = get_addr(host, port, SOCK_STREAM);
-
-    /* Cf. getaddrinfo(3). */
-    for (rp = result; rp != NULL; rp = rp->ai_next) {
-        fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-        if (fd == -1) {
-            perror("connect_tcp_sock socket()");
-            continue;
-        }
-
-        if (connect(fd, rp->ai_addr, rp->ai_addrlen) == 0)
-            break;                  /* Success */
-
-        perror("connect_tcp_sock connect()");
-        close(fd);
-    }
-
-    if (rp == NULL) {               /* No address succeeded */
-        fprintf(stderr, "Could not connect %s:%s\n", host, port);
-        exit(EXIT_FAILURE);
-    }
-
-    freeaddrinfo(result);
-    return fd;
-}
-
-void send_or_die(int fd, char *buf, size_t len) {
-    ssize_t nw;
-
-    while (len != 0 && (nw = send(fd, buf, len, 0)) != 0) {
-        if (nw == -1) {
-            if (errno == EINTR)
-                continue;
-            else {
-                fprintf(stderr, "writing to fd %d: %s\n", fd, strerror(errno));
-                exit(EXIT_FAILURE);
-            }
-        }
-        len -= nw;
-        buf += nw;
-    }
-}
-
